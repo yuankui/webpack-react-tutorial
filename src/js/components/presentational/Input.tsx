041375e12a9d1fb2af0f910053f81c27386c4c5c
yuankui <yuankui@meituan.com>
2019-09-26 17:16:37 +0800

    add codemirror for test

--- src/js/components/presentational/Input.tsx
+++ src/js/components/presentational/Input.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 import {ChangeEventHandler} from "react";
+import {UnControlled as CodeMirror} from 'react-codemirror2'
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+require('codemirror/mode/xml/xml');
+require('codemirror/mode/javascript/javascript');
 
 interface Props {
   label: string,
@@ -15,13 +20,16 @@ export default class Input extends React.Component<Props, any> {
     const {label, text, type, id, value, handleChange} = this.props;
     return <div className="form-group">
       <label htmlFor={label}>{text}</label>
-      <input
-        type={type}
-        className="form-control"
-        id={id}
-        value={value}
-        onChange={handleChange}
-        required
+
+      <CodeMirror
+        value='<h1>I ♥ react-codemirror2</h1>'
+        options={{
+          mode: 'xml',
+          theme: 'material',
+          lineNumbers: true
+        }}
+        onChange={(editor, data, value) => {
+        }}
       />
     </div>
   }

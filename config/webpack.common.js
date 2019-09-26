a43ce05eb9f040b06f865075249eaf2866538d95
yuankui <yuankui@meituan.com>
2019-09-26 15:25:50 +0800

    common

---
+++ config/webpack.common.js
@@ -0,0 +1,54 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 'css-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+
+  devtool: "source-map",
+  devServer: {
+    hot: true,
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,11 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const merge = require('webpack-merge');
+const common = require('./config/webpack.common');
+const path = require('path');
 
 
 module.exports = {
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".jsx"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "ts-loader"
-          }
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader', 'css-loader'
-        ]
-      },
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ],
-
-  devtool: "source-map",
-  devServer: {
-    hot: true,
+  output: {
   }
 };
+
+module.exports = merge(common, module.exports);
